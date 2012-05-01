c1aae7ea555a18b7bccf51669b87b91899bdac29
Sami <sami@01.fi>
2012-05-01 09:00:40 +0300

    Initial commit

---
+++ OmaKaupunki/controller/Dataprovider.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Text;
+using OmaKaupunki.model;
+using System.Collections.ObjectModel;
+using System.Runtime.Serialization.Json;
+
+/**
+ * http://api.omakaupunki.fi/v1/event?api_key=262d6328-82f7-11e1-a468-000c29f7271d
+http://api.omakaupunki.fi/v1/event/categories?api_key=262d6328-82f7-11e1-a468-000c29f7271d
+http://api.omakaupunki.fi/v1/event/categories/108?api_key=262d6328-82f7-11e1-a468-000c29f7271d
+http://api.omakaupunki.fi/v1/event/search?start_date=2012-04-12&category=9&api_key=262d6328-82f7-11e1-a468-000c29f7271d
+
+ * JSON
+ * http://pietschsoft.com/post/2008/02/NET-35-JSON-Serialization-using-the-DataContractJsonSerializer.aspx
+ * http://www.smallandmighty.net/blog/using-json-with-windows-phone
+ * http://debugmode.net/2011/12/22/how-to-consume-wcf-rest-service-with-json-in-windows-phone-7/
+ * http://msdn.microsoft.com/en-us/library/bb410770.aspx
+ * http://msdn.microsoft.com/en-us/library/system.runtime.serialization.json.datacontractjsonserializer%28v=vs.95%29.aspx
+ * http://msdn.microsoft.com/en-us/library/system.runtime.serialization.datacontractjsonserializer%28v=VS.90%29.aspx?appId=Dev10IDEF1&l=EN-US&k=k%28DATACONTRACTJSONSERIALIZER%29&rd=true
+ * http://msdn.microsoft.com/en-us/library/system.runtime.serialization.json.datacontractjsonserializer%28v=vs.95%29.aspx
+ * http://msdn.microsoft.com/en-us/library/bb412179.aspx
+ * /
+namespace OmaKaupunki.controller
+{
+    public class Dataprovider
+    {
+        public void test(){
+            Categories category = downloadCategories(new Uri("http://api.omakaupunki.fi/v1/event/categories?api_key=262d6328-82f7-11e1-a468-000c29f7271d"));
+            MessageBox.Show((category!=null)? "ok": "EPIC FAIL!!!");
+        }
+
+
+        private Categories downloadCategories(Uri uri)
+        {
+            Categories categories = new Categories();
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.DownloadStringCompleted += new DownloadStringCompletedEventHandler(downloadCategoriesCompleted);
+                webClient.DownloadStringAsync(uri);
+            }
+            catch
+            {
+                MessageBox.Show("Can not download events");
+            }
+            return categories;
+        }
+
+        private void downloadCategoriesCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            try
+            {
+                MemoryStream memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(e.Result));
+                DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(Categories));
+                Categories categories = (Categories)serializer.ReadObject(memoryStream);
+                if(categories == null)
+                    throw exception;
+            }
+            catch(Exception ex) {
+                //TODO error
+            }
+        }
+
+        private void downloadCompleted_(object sender, DownloadStringCompletedEventArgs e)
+        {
+            try
+            {
+                MemoryStream memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(e.Result));
+                ObservableCollection<Category> categories = new ObservableCollection<Category>();
+                DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(ObservableCollection<Category>));
+                categories = (ObservableCollection<Category>)serializer.ReadObject(memoryStream);
+                /*foreach (Beacon b in list)
+                {
+                    RideList.Add(new RideDataModel { Date = b.Timestamp.ToString(), Description = b.Description, Distance = b.DistanceFromCustomer.ToString(), Name = b.Id.ToString(), Location = string.Format("{0},{1}", b.Location.Latitude.ToString(), b.Location.Longitude.ToString()), Address = b.Location.Address });
+                }*/
+                MessageBox.Show((categories.Count != 0) ? "ok" : "EPIC FAIL!!!");
+
+                MessageBox.Show(e.Result);
+            }
+            catch
+            {
+                //TODO error
+            }
+        }
+    }
+}
---
+++ OmaKaupunki/views/Browse.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace OmaKaupunki
+{
+    public partial class Browse : PhoneApplicationPage
+    {
+        public Browse()
+        {
+            InitializeComponent();
+        }
+
+        private void search(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/search.xaml?pivot.SelectedIndex=" + pivot.SelectedIndex, UriKind.Relative));
+        }
+    }
+}
