9fac2b880e0c1081d98531738c999736af52f6c3
Sami <hostikka@gmail.com>
2012-05-02 18:12:33 +0300

    Event works almost

--- OmaKaupunki/views/Event.xaml.cs
+++ OmaKaupunki/views/Event.xaml.cs
@@ -10,14 +10,97 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using System.Windows.Navigation;
+using OmaKaupunki.model;
+using OmaKaupunki.controller;
+using Newtonsoft.Json;
+using Microsoft.Phone.Controls.Maps;
+using System.Device.Location;
+using Microsoft.Phone.Tasks;
 
 namespace OmaKaupunki
 {
     public partial class Event : PhoneApplicationPage
     {
+        int id;
+        string uri = "";
+
         public Event()
         {
             InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            string tmp;
+
+            if (NavigationContext.QueryString.TryGetValue("id", out tmp) && int.TryParse(tmp, out id))
+                download();
+        }
+
+        private void download()
+        {
+            try
+            {
+                Dataprovider dataprovider = new Dataprovider();
+                WebClient webClient = new WebClient();
+                webClient.DownloadStringCompleted += new DownloadStringCompletedEventHandler(downloadCompleted);
+                webClient.DownloadStringAsync(new Uri(dataprovider.APIURL + id + "?api_key=" + dataprovider.APIKEY));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                err.Visibility = System.Windows.Visibility.Visible;
+            }
+        }
+
+        private void downloadCompleted(object sender, DownloadStringCompletedEventArgs ex)
+        {
+            try
+            {
+                model.Events data = JsonConvert.DeserializeObject<model.Events>(ex.Result);
+                model.Event e = data.data.First();
+                if (e == null)
+                    err.Visibility = System.Windows.Visibility.Visible;
+                else
+                {
+                    title.Text = e.title;
+                    body.Text = e.body;
+                    if (e.url != null && e.url != "")
+                    {
+                        url.Content = e.url;
+                        uri = e.url; ;
+                    }
+
+                    startTime.Text = e.start_time.ToString();
+                    GeoCoordinate geoCoordinate = e.toGeoCoordinate();
+                    Pushpin pushpin = new Pushpin();
+                    pushpin.Location = geoCoordinate;
+
+                    smallMap.Center = geoCoordinate;
+                    smallMap.Children.Add(pushpin);
+                    map.Children.Add(pushpin);
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.ToString());
+                err.Visibility = System.Windows.Visibility.Visible;
+            }
+            finally
+            {
+                performanceProgressBar.IsIndeterminate = false;
+            }
+        }
+
+        private void url_Click(object sender, RoutedEventArgs e)
+        {
+            if (uri != "")
+            {
+                WebBrowserTask webBrowserTask = new WebBrowserTask();
+                webBrowserTask.Uri = new Uri(uri, UriKind.Absolute);
+                webBrowserTask.Show();
+            }
         }
     }
 }
